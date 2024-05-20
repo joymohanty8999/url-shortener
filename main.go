4a72aca05df84de41ab99b2c334bf5381d456b9c
Joseph Mohanty <jm215@rice.edu>
2024-05-20 13:34:15 -0500

    main go file

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"url-shortener/handlers"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/shorten", handlers.ShortenURL).Methods("POST")
+	router.HandleFunc("/{shortURL}", handlers.RetrieveURL).Methods("GET")
+
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
