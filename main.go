96f6b002753dc58fdfae5cd94e97b2ee216fbb7f
Joseph Mohanty <jm215@rice.edu>
2024-05-21 12:52:43 -0500

    Updating /check handle

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 
 	router.HandleFunc("/shorten", handlers.ShortenURL).Methods("POST")
 	router.HandleFunc("/{shortURL}", handlers.RetrieveURL).Methods("GET")
+	router.HandleFunc("/check", handlers.CheckURL).Methods("POST")
 
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
