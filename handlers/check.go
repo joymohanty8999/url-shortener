70c8680218bcee994b008a80ca10487c3df5009c
Joseph Mohanty <jm215@rice.edu>
2024-05-21 12:48:35 -0500

    Check the URL mappings for expiration

---
+++ handlers/check.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+	"url-shortener/models"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type CheckRequest struct {
+	URL string `json:"url"`
+}
+
+type CheckResponse struct {
+	Exists   bool   `json:"exists"`
+	ShortURL string `json:"short_url,omitempty"`
+	Expired  *bool  `json:"expired,omitempty"`
+}
+
+func CheckURL(w http.ResponseWriter, r *http.Request) {
+	var request CheckRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var existingURL models.URL
+	filter := bson.M{"original_url": request.URL}
+	opts := options.FindOne().SetSort(bson.D{{Key: "expiration", Value: -1}})
+	err := urlCollection.FindOne(context.TODO(), filter, opts).Decode(&existingURL)
+	if err != nil {
+		// URL not found in the database
+		response := CheckResponse{Exists: false}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	// URL found, check if it is expired
+	expired := time.Now().After(existingURL.Expiration)
+	response := CheckResponse{
+		Exists:   true,
+		ShortURL: baseURL + existingURL.ShortURL,
+		Expired:  &expired,
+	}
+	json.NewEncoder(w).Encode(response)
+}
