b4d9f686242fa3b5960c8cf2aedf2247756ef838
Joseph Mohanty <jm215@rice.edu>
2024-05-22 09:56:22 -0500

    Adding default handler

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ import (
 func main() {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome to the URL Shortener API. Use /shorten, /{shortURL}, /check endpoints"))
+	})
+
+	router.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./favicon.ico")
+	})
+
 	router.HandleFunc("/shorten", handlers.ShortenURL).Methods("POST")
 	router.HandleFunc("/{shortURL}", handlers.RetrieveURL).Methods("GET")
 	router.HandleFunc("/check", handlers.CheckURL).Methods("POST")
