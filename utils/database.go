6363aa05c602cbf0d76bdb423cb8213304453975
Joseph Mohanty <jm215@rice.edu>
2024-05-21 21:35:14 -0500

    Update MongoDB connection settings

--- utils/database.go
+++ utils/database.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -11,32 +12,32 @@ import (
 
 var Client *mongo.Client
 
-func InitDB() {
-	Client = ConnectDB()
-}
-
 func ConnectDB() *mongo.Client {
 	mongoURI := os.Getenv("MONGODB_URI")
 	clientOptions := options.Client().ApplyURI(mongoURI)
 
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = client.Ping(context.TODO(), nil)
+	err = client.Ping(ctx, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println("Connected to MongoDB!")
 	return client
-
 }
 
 func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-
 	collection := client.Database("url_shortener").Collection(collectionName)
 	return collection
+}
 
+func InitDB() {
+	Client = ConnectDB()
 }
