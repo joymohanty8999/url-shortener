966fc64fb3648ea8b83af49f25ccaa409600a1b0
Joseph Mohanty <jm215@rice.edu>
2024-05-21 21:40:25 -0500

    Update MongoDB connection settings

--- utils/database.go
+++ utils/database.go
@@ -14,19 +14,23 @@ var Client *mongo.Client
 
 func ConnectDB() *mongo.Client {
 	mongoURI := os.Getenv("MONGODB_URI")
-	clientOptions := options.Client().ApplyURI(mongoURI)
+	if mongoURI == "" {
+		log.Fatal("MONGODB_URI environment variable not set")
+	}
+
+	clientOptions := options.Client().ApplyURI(mongoURI).SetServerSelectionTimeout(10 * time.Second)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
 	}
 
 	err = client.Ping(ctx, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to ping MongoDB: %v", err)
 	}
 
 	log.Println("Connected to MongoDB!")
