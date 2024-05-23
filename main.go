390a1db0f2cf9456852254da4920803ae2eaaf83
Joseph Mohanty <jm215@rice.edu>
2024-05-22 21:05:43 -0500

    Adding detailed logging

--- main.go
+++ main.go
@@ -15,16 +15,20 @@ func main() {
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Welcome to the URL Shortener API. Use /shorten, /{shortURL}, /check, /urls endpoints"))
-	})
+		log.Println("Root Endpoint hit")
+	}).Methods("GET")
 
 	router.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./favicon.ico")
-	})
+		log.Println("Favicon endpoint hit")
+	}).Methods("GET")
 
 	router.HandleFunc("/shorten", handlers.ShortenURL).Methods("POST")
 	router.HandleFunc("/{shortURL}", handlers.RetrieveURL).Methods("GET")
 	router.HandleFunc("/check", handlers.CheckURL).Methods("POST")
+
 	router.HandleFunc("/urls", handlers.GetAllURLs).Methods("GET")
+	log.Println("Registered /urls endpoint")
 
 	port := os.Getenv("PORT")
 
