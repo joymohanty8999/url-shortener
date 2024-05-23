93cb1e3f7786d8fa099ff55881c6bfc15c2ba858
Joseph Mohanty <jm215@rice.edu>
2024-05-22 20:11:32 -0500

    Adding endpoint to retrieve all generated short urls

---
+++ handlers/getall.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+	"url-shortener/models"
+	"url-shortener/utils"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func GetAllURLs(w http.ResponseWriter, r *http.Request) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cursor, err := utils.Client.Database("url_shortener").Collection("urls").Find(ctx, bson.M{})
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer cursor.Close(ctx)
+
+	var urls []models.URL
+
+	if err = cursor.All(ctx, &urls); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(urls)
+
+}
