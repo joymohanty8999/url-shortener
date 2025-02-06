d3df6995c748840c77238f7c56939018b1073aab
Joseph Mohanty <jm215@rice.edu>
2025-02-06 10:19:19 -0600

    Update base URL

--- handlers/shorten.go
+++ handlers/shorten.go
@@ -14,7 +14,7 @@ import (
 
 var urlCollection *mongo.Collection
 
-const baseURL = "https://snip-go-and-dash.herokuapp.com/api/"
+const baseURL = "https://snip-snip-go.herokuapp.com/api/"
 const expirationDuration = 2 * time.Hour //adding a time of 2 hours per short url generated
 
 type ShortenRequest struct {
