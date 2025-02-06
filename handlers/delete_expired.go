0e5abbcb2a10b33a006aea700e1d53061dba0be7
Joseph Mohanty <jm215@rice.edu>
2025-02-06 11:14:12 -0600

    Fix: Delete expired URLs based on timestamp

--- handlers/delete_expired.go
+++ handlers/delete_expired.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 	"url-shortener/utils"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -12,7 +13,8 @@ import (
 func DeleteExpiredURLs(w http.ResponseWriter, r *http.Request) {
 	collection := utils.Client.Database("url-shortener").Collection("urls")
 
-	result, err := collection.DeleteMany(r.Context(), bson.M{"expired": true})
+	// Delete URLs where the expiration timestamp has passed
+	result, err := collection.DeleteMany(r.Context(), bson.M{"expiration": bson.M{"$lt": time.Now()}})
 	if err != nil {
 		log.Printf("Error deleting expired URLs: %v", err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
